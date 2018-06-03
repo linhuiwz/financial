3ee6b8e16b129f185b55707658fa01937ac56969
linhuiwz <linhui_wz@163.com>
2018-06-03 19:40:49 +0800

    6/3 RSA

--- seller/src/main/java/com/imooc/seller/SellerApp.java
+++ seller/src/main/java/com/imooc/seller/SellerApp.java
@@ -2,10 +2,13 @@ package com.imooc.seller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cache.annotation.EnableCaching;
+
 
 @SpringBootApplication
 @EnableCaching
+@EntityScan(basePackages = {"com.imooc.entity"})
 public class SellerApp {
 
     public static void main(String[] args) {
---
+++ seller/src/main/java/com/imooc/seller/controller/OrderController.java
@@ -0,0 +1,32 @@
+package com.imooc.seller.controller;
+
+import com.imooc.entity.Order;
+import com.imooc.seller.service.OrderService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+
+    private static Logger LOG = LoggerFactory.getLogger(OrderController.class);
+
+    @Autowired
+    private OrderService orderService;
+
+    @RequestMapping(value = "/apply", method = RequestMethod.POST)
+    public Order apply(@RequestBody Order order) {
+
+        LOG.info("申购请求:{}", order);
+        order = orderService.apply(order);
+        LOG.info("申购结果:{}", order);
+
+        return order;
+    }
+
+}
