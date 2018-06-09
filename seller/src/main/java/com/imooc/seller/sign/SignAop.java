648510554785c827977807a1f25230f16aca0096
linhuiwz <linhui_wz@163.com>
2018-06-09 10:55:19 +0800

    6/9 order apply rsa feature

---
+++ seller/src/main/java/com/imooc/seller/sign/SignAop.java
@@ -0,0 +1,28 @@
+package com.imooc.seller.sign;
+
+import com.imooc.seller.service.SignService;
+import com.imooc.util.RSAUtil;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
+
+/**
+ * 验签aop
+ */
+@Component
+@Aspect
+public class SignAop {
+
+    @Autowired
+    private SignService signService;
+
+    @Before(value = "execution(* com.imooc.seller.controller.*.*(..)) && args(authId,sign,text,..)")
+    public void verify(String authId, String sign, SignText text) {
+        //authId应该保存在数据库或者配置文件，由对方提供，这里简化。
+        String publicKey = signService.getPublicKey(authId);
+        Assert.isTrue(RSAUtil.verify(text.toText(), sign, publicKey), "验签失败！");
+    }
+
+}
