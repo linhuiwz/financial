d44767689e9450d9ffb3d0b31ecea2916faa9737
linhui_wz <linhui_wz@163.com>
2018-06-26 10:29:56 +0800

    lombok

---
+++ manager/src/test/java/com/imooc/lombok/Chapter29ApplicationTests.java
@@ -0,0 +1,22 @@
+package com.imooc.lombok;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@Slf4j
+public class Chapter29ApplicationTests {
+
+    @Test
+    public void testLombok() {
+        //测试Getter/Setter
+        UserBean user = new UserBean();
+        user.setName("测试lombok");
+        System.out.println(user.getName());
+    }
+
+}
